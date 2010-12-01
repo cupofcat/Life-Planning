83589d64e282d111e3e2cb20d49bc2353aa001f3
Aga Madurska <amadurska@gmail.com>
2010-12-01 15:04:51 +0000

    Base for suggestion servlet

---
+++ src/com/appspot/iclifeplanning/SettingsServlet.java
@@ -0,0 +1,47 @@
+package com.appspot.iclifeplanning;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.appspot.datastore.UserProfile;
+import com.appspot.datastore.UserProfileStore;
+import com.google.appengine.repackaged.org.json.JSONException;
+import com.google.appengine.repackaged.org.json.JSONObject;
+
+/**
+ * Settings servlet. 
+ * 
+ * @author Agnieszka Magda Madurska (amm208@doc.ic.ac.uk)
+ * 
+ */
+@SuppressWarnings("serial")
+public class SettingsServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+		throws IOException {
+
+		JSONObject settingsJSON = null;
+
+		try {
+			settingsJSON = new JSONObject(request.getReader().readLine());
+			String userID = settingsJSON.getString("userID");
+			boolean fullOpt = settingsJSON.getBoolean("fullOpt");
+			
+			UserProfile userProfile = UserProfileStore.getUserProfile(userID);
+			userProfile.setFullyOptimized(fullOpt);
+			
+			userProfile.makePersistent();
+		} catch (JSONException e) {
+			System.out.println("Badly formatted JSON!");
+			e.printStackTrace();
+		}
+	}
+}
