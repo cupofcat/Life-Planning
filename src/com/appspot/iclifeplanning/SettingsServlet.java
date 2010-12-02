7f1e61bf531064ed9b1c449df9b03a04130ddd2b
Aga Madurska <amadurska@gmail.com>
2010-12-02 20:23:59 +0000

    Building a structer for storing all the preferences oft he user over time

--- src/com/appspot/iclifeplanning/SettingsServlet.java
+++ src/com/appspot/iclifeplanning/SettingsServlet.java
@@ -1,11 +1,13 @@
 package com.appspot.iclifeplanning;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.appspot.datastore.SphereName;
 import com.appspot.datastore.UserProfile;
 import com.appspot.datastore.UserProfileStore;
 import com.google.appengine.repackaged.org.json.JSONException;
@@ -34,7 +36,8 @@ public class SettingsServlet extends HttpServlet {
 			settingsJSON = new JSONObject(request.getReader().readLine());
 			String userID = settingsJSON.getString("userID");
 			boolean fullOpt = settingsJSON.getBoolean("fullOpt");
-			
+			// TODO(amadurska): Get this from JSON
+			HashMap<SphereName, Double> spherePreferences = new HashMap<SphereName, Double>();
 			UserProfile userProfile = UserProfileStore.getUserProfile(userID);
 			userProfile.setFullyOptimized(fullOpt);
 			
