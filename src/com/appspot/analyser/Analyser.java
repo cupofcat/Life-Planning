c9dc32fe5fc3a2c514325838b91f0fbbe1c126b0
bober02 <msbobrowski@gmail.com>
2011-01-14 11:55:30 +0000

    New version

--- src/com/appspot/analyser/Analyser.java
+++ src/com/appspot/analyser/Analyser.java
@@ -73,7 +73,7 @@ public class Analyser {
 		Calendar endFamilyDate = new GregorianCalendar(2000, 0, 3, 16, 0, 0);
 		Pair<Calendar, Calendar> possibleFamilySlot = new Pair<Calendar, Calendar>(startFamilyDate, endFamilyDate);
 		Map<SphereName, Double> sphereFamilyInfluences = Utilities.generateSpheres(new double[] { 0.0, 0.0, 1.0 });
-		Proposal family = new Proposal("Family dinner", "Stejki - masa sie liczy", null, null, 20, 60, false, true, sphereFamilyInfluences);
+		Proposal family = new Proposal("Family dinner", "Steaks - all that matters", null, null, 20, 60, false, true, sphereFamilyInfluences);
 		family.setPossibleTimeSlot(possibleFamilySlot);
 		family.makePersistent();
 		LinkedList<Proposal> families = new LinkedList<Proposal>();
@@ -92,7 +92,7 @@ public class Analyser {
 		Calendar endRecreationDate2 = new GregorianCalendar(2000, 0, 3, 22, 30, 0);
 		Pair<Calendar, Calendar> possibleRecreationSlot2 = new Pair<Calendar, Calendar>(startRecreationDate2, endRecreationDate2);
 		Map<SphereName, Double> sphereRecreationInfluences2 = Utilities.generateSpheres(new double[] { 0.0, 0.0, 0.0, 1.0 });
-		Proposal recreation2 = new Proposal("Beer with a friend", "Jeszcze po kropelce", null, null, 20, 60, false, true, sphereRecreationInfluences2);
+		Proposal recreation2 = new Proposal("Beer with a friend", "Take 2KC with me", null, null, 20, 60, false, true, sphereRecreationInfluences2);
 		recreation2.setPossibleTimeSlot(possibleRecreationSlot2);
 		recreation2.makePersistent();
 		
@@ -100,15 +100,42 @@ public class Analyser {
 		Calendar endRecreationDate3 = new GregorianCalendar(2000, 0, 3, 22, 30, 0);
 		Pair<Calendar, Calendar> possibleRecreationSlot3 = new Pair<Calendar, Calendar>(startRecreationDate3, endRecreationDate3);
 		Map<SphereName, Double> sphereRecreationInfluences3 = Utilities.generateSpheres(new double[] { 0.0, 0.0, 0.0, 1.0 });
-		Proposal recreation3 = new Proposal("Poker session", "Co bym se nie wygral...", null, null, 20, 60, false, true, sphereRecreationInfluences3);
+		Proposal recreation3 = new Proposal("Poker session", "Royal flush - here I come", null, null, 20, 60, false, true, sphereRecreationInfluences3);
 		recreation3.setPossibleTimeSlot(possibleRecreationSlot3);
 		recreation3.makePersistent();
+		
+		Calendar startRecreationDate4 = new GregorianCalendar(2000, 0, 3, 20, 0, 0);
+		Calendar endRecreationDate4 = new GregorianCalendar(2000, 0, 3, 21, 30, 0);
+		Pair<Calendar, Calendar> possibleRecreationSlot4 = new Pair<Calendar, Calendar>(startRecreationDate4, endRecreationDate4);
+		Map<SphereName, Double> sphereRecreationInfluences4 = Utilities.generateSpheres(new double[] { 0.0, 0.0, 0.0, 1.0 });
+		Proposal recreation4 = new Proposal("Go fly fishing", "Hope to catch a halibut :)", null, null, 20, 60, false, true, sphereRecreationInfluences4);
+		recreation4.setPossibleTimeSlot(possibleRecreationSlot4);
+		recreation4.makePersistent();
+		
+		Calendar startRecreationDate5 = new GregorianCalendar(2000, 0, 3, 20, 0, 0);
+		Calendar endRecreationDate5 = new GregorianCalendar(2000, 0, 3, 20, 30, 0);
+		Pair<Calendar, Calendar> possibleRecreationSlot5 = new Pair<Calendar, Calendar>(startRecreationDate5, endRecreationDate5);
+		Map<SphereName, Double> sphereRecreationInfluences5 = Utilities.generateSpheres(new double[] { 0.0, 0.0, 0.0, 1.0 });
+		Proposal recreation5 = new Proposal("Afternoon nap", "Not taking any calls", null, null, 20, 60, false, true, sphereRecreationInfluences5);
+		recreation5.setPossibleTimeSlot(possibleRecreationSlot5);
+		recreation5.makePersistent();
+		
+		Calendar startRecreationDate6 = new GregorianCalendar(2000, 0, 3, 19, 0, 0);
+		Calendar endRecreationDate6 = new GregorianCalendar(2000, 0, 3, 20, 30, 0);
+		Pair<Calendar, Calendar> possibleRecreationSlot6 = new Pair<Calendar, Calendar>(startRecreationDate6, endRecreationDate6);
+		Map<SphereName, Double> sphereRecreationInfluences6 = Utilities.generateSpheres(new double[] { 0.0, 0.0, 0.0, 1.0 });
+		Proposal recreation6 = new Proposal("Have a smoke", "Co mowi palma do palmy? Za palmy!", null, null, 20, 60, false, true, sphereRecreationInfluences6);
+		recreation6.setPossibleTimeSlot(possibleRecreationSlot6);
+		recreation6.makePersistent();
 		
 		
 		LinkedList<Proposal> recreations = new LinkedList<Proposal>();
 		recreations.add(recreation);
 		recreations.add(recreation2);
 		recreations.add(recreation3);
+		//recreations.add(recreation4);
+		//recreations.add(recreation5);
+		//recreations.add(recreation6);
 		proposals.put(SphereName.RECREATION, recreations);
 	}
 
@@ -133,7 +160,6 @@ public class Analyser {
 		if (isCloseEnough(start, optimizeFull))
 			return result;
 		removeStaticEvents(events);
-
 		List<CalendarStatus> statuses = Utilities.merge( generateProposalStatuses(start.getDeficitSpheres(optimizeFull), start, true) , 
 				generateEventStatuses(events, start)
 				);
